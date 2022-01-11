d4b381850fd98d4879477c3989c80a7faaff25e2
krdheeraj51 <krdheeraj51@gmail.com>
2022-01-11 08:01:11 +0530

    added algorithm first day code.

---
+++ Algorithm/Day1/ConvertCelciusToFarenheit.js
@@ -0,0 +1,9 @@
+/**
+ * The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+ */
+ function convertToF(celsius) {
+    let fahrenheit= (celsius * 9/5)+32;
+    return fahrenheit;
+  }
+  
+  convertToF(30);
