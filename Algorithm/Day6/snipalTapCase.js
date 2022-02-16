971eb953bae7f9eefb09ed35fe6d4d6c9ff8b16f
krdheeraj51 <krdheeraj51@gmail.com>
2022-02-16 21:13:25 +0530

    day 6 code practice.

---
+++ Algorithm/Day6/snipalTapCase.js
@@ -0,0 +1,9 @@
+/**
+ * Spinal Tap Case
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+ */
+
+function spinalCase(str) {
+  return str.split(/\s|_|(?=[A-Z])/).join('-').toLowerCase();
+}
+spinalCase('AllThe-small Things')
